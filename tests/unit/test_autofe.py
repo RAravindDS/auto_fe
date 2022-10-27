bdd40a175905e168430dc72a7cddaca1256cf1c2
RAravindDS <aravindan22052001@gmail.com>
2022-10-27 12:37:51 +0530

    changed path for train csv

--- tests/unit/test_autofe.py
+++ tests/unit/test_autofe.py
@@ -2,7 +2,7 @@ import pytest
 from AutoFeatureEnginerring import AutoFeatureEngineering
 import pandas as pd
 
-data = pd.read_csv(r"D:\FinalPYpi\auto_fe\auto_fe\research\train.csv")
+data = pd.read_csv(r"tests\unit\train.csv")
 out = AutoFeatureEngineering(data).auto_fe()
 print(out.isnull().sum())
 
